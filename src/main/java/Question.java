ff8d2c20fa70b5ae7093c030142dcce50710ae59
lm222ix <lm222ix@student.lnu.se>
2015-11-16 13:26:35 +0100

    Class QUestion and testclass for it created

---
+++ src/main/java/Question.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Ludde on 2015-11-16.
+ */
+public class Question {
+    
+}
---
+++ src/test/java/QuestionClassTests.java
@@ -0,0 +1,13 @@
+import junit.framework.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static junit.framework.Assert.*;
+
+/**
+ * Created by Ludde on 2015-11-13.
+ */
+public class QuestionClassTests {
+
+
+
+}
