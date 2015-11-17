094208fd49ef754647aba88cc1ec089e871ab6b3
lm222ix <lm222ix@student.lnu.se>
2015-11-17 16:13:14 +0100

    RED - Question test #12 - CalculateAnswer with minus operator

---
+++ src/test/java/QuestionClassCalculateAnswerTests/CalculateAnswerWithMinusOperator.java
@@ -0,0 +1,40 @@
+package QuestionClassCalculateAnswerTests;
+
+import Model.Question;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by Ludde on 2015-11-17.
+ */
+public class CalculateAnswerWithMinusOperator {
+
+    private static Question q;
+    private static ArrayList<Integer> testList;
+
+    @BeforeClass
+    public static void setUp() {
+        testList = new ArrayList<Integer>(
+                Arrays.asList(5, 3)
+        );
+        q = new Question() {
+            @Override
+            public void makeNewQuestion(int diff) {
+                setOperator('-');
+                setNumbers(testList);
+                setAnswer(calculateAnswer());
+            }
+        };
+    }
+
+    @Test
+    public void shouldCalculateCorrectWithMinusOperatorDiffOne() {
+        q.makeNewQuestion(1);
+        assertEquals(2, q.getAnswer());
+    }
+}
